dc6ce6f5441ac173217ec02301be18be8e0253c1
Prateek <prateekj212@gmail.com>
2014-05-05 13:20:25 +0530

    Basic layout design. Adding files ddiffjs.js and demo.html

---
+++ ddiffjs.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function newWindow(){
+    LeapWindow = window.open("../../leapSite/assets/images/Global_construction_3.jpg","Testing ddiffjs","width=980,height=500");
+}
